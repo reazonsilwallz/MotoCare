f7f3e4e59a010ad398f830f50041b4d75a1fe4f6
Reazon <reazonsilwal24@gmail.com>
2026-04-29 08:32:45 -0400

    add title and caption for sidebar

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-
+# Set page configuration such as title, icon and layout
 st.set_page_config(page_title="MotoCare", page_icon="assets/logo.png", layout="wide")
 
-
+#title of the app
 st.title("MotoCare: Your Ultimate Car Maintenance Companion")
 
-df = pd.read_csv("assets/datasets.csv")
-st.dataframe(df)
+# SIDE BAR
+st.sidebar.header("MotoCare")
+st.sidebar.markdown("Track - Maintain - Drive Safe")
 
-st.subheader("Health Score by Vehicle")
-st.bar_chart(df.set_index("CarName")["HealthScore"])
+
+
+
+
+
+
+
+
+# df = pd.read_csv("assets/datasets.csv")
+# st.dataframe(df)
+
+# st.subheader("Health Score by Vehicle")
+# st.bar_chart(df.set_index("CarName")["HealthScore"])
