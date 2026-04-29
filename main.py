314d87b3a3007741c1560ceefab3cd908eb456c7
Reazon <reazonsilwal24@gmail.com>
2026-04-29 08:02:55 -0400

    test sample data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 
 
 st.set_page_config(page_title="MotoCare", page_icon="assets/logo.png", layout="wide")
@@ -7,3 +7,5 @@ st.set_page_config(page_title="MotoCare", page_icon="assets/logo.png", layout="w
 
 st.title("MotoCare: Your Ultimate Car Maintenance Companion")
 
+df = pd.read_csv("assets/sample_data.csv")
+st.dataframe(df)
