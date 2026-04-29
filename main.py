3544a41336ffd64ba07d164fc1cc419bc321b274
Reazon <reazonsilwal24@gmail.com>
2026-04-29 07:37:39 -0400

    add logo

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="MotoCare", page_icon=":car:", layout="wide")
 
-st.write("Hello, welcome to MotoCare!")
 
+st.set_page_config(page_title="MotoCare", page_icon="assets/logo.png", layout="wide")
+
+
+st.title("MotoCare: Your Ultimate Car Maintenance Companion")
 
