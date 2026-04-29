ee066846f7cdf69427e7014034963f43676665af
Reazon <reazonsilwal24@gmail.com>
2026-04-29 08:25:19 -0400

    test some datasets

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ st.set_page_config(page_title="MotoCare", page_icon="assets/logo.png", layout="w
 
 st.title("MotoCare: Your Ultimate Car Maintenance Companion")
 
-df = pd.read_csv("assets/sample_data.csv")
+df = pd.read_csv("assets/datasets.csv")
 st.dataframe(df)
+
+st.subheader("Health Score by Vehicle")
+st.bar_chart(df.set_index("CarName")["HealthScore"])
