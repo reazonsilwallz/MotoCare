d45aa0c2aea60ee17da195a57a6dd63a4e0dafb0
Reazon <reazonsilwal24@gmail.com>
2026-04-29 08:40:54 -0400

    add feature description

--- main.py
+++ main.py
@@ -11,7 +11,23 @@ st.title("MotoCare: Your Ultimate Car Maintenance Companion")
 st.sidebar.header("MotoCare")
 st.sidebar.markdown("Track - Maintain - Drive Safe")
 
+page=st.sidebar.radio(
+    "Navigation",
+    ("Home", "Maintenance Schedule", "Health Score", "Service History", "Tips & Resources")
+)
 
+st.write(
+"Monitor vehicle health, track maintenance, and keep your car running at peak performance."
+)
+
+
+# App Description
+st.subheader("Features")
+
+st.write("1. Track vehicle health")
+st.write("2. Log maintenance history")
+st.write("3. Service reminders")
+st.write("4. View analytics")
 
 
 
